9924583ec781cc3ee6f876546bf448946a9f45a9
notrabs <35877960+notrabs@users.noreply.github.com>
2023-11-12 16:41:13 +0100

    Add Test for Studio Functions

---
+++ Test/StudioObject/StudioFunctions.rrcg.cs
@@ -0,0 +1,38 @@
+﻿using RRCGSource;
+using UnityEngine;
+
+public class StudioFunctions : CircuitDescriptor
+{
+    public override void CircuitGraph()
+    {
+        var studioObject = new StudioFunctionsObject(RecRoomObject.Invalid);
+
+        studioObject.VoidFunction();
+
+        studioObject.AllParameterTypes(true, 23f, 7, "test", new Color(1.0f, 0, 0), Vector3.up, Quaternion.identity);
+
+        ChipLib.Log(studioObject.SingleReturn(Vector3.zero));
+
+        var multiReturn = studioObject.MultiReturn();
+        ChipLib.Log(multiReturn.Item1);
+        ChipLib.Log(multiReturn.Item2);
+    }
+}
+
+public class StudioFunctionsObject : StudioObjectDescriptor
+{
+    public StudioFunctionsObject(StudioObject target) : base(target, "TestPrefab") { }
+
+    [ExistingStudioFunction]
+    public void VoidFunction() { }
+
+    [ExistingStudioFunction]
+    public void AllParameterTypes(bool p0, float p1, int p2, string p3, Color p4, Vector3 p5, Quaternion p6) { }
+
+    [ExistingStudioFunction]
+    public Vector3 SingleReturn(Vector3 input) { return default; }
+
+    [ExistingStudioFunction]
+    public (Vector3, Quaternion) MultiReturn() { return default; }
+}
+
---
+++ Test/StudioObject/StudioFunctions.rrcg.gen.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using RRCGBuild;
+using System.Collections.Generic;
+
+namespace RRCGBuild
+{
+    public class StudioFunctions : CircuitBuilder
+    {
+        public override void CircuitGraph()
+        {
+            ExecFlow rrcg_return_flow = new ExecFlow();
+            var studioObject = new StudioFunctionsObject(RecRoomObjectPort.Invalid);
+            studioObject.VoidFunction();
+            studioObject.AllParameterTypes(true, 23f, 7, "test", new ColorPort(1.0f, 0, 0), Vector3Port.up, QuaternionPort.identity);
+            ChipLib.Log(studioObject.SingleReturn(Vector3Port.zero));
+            var multiReturn = studioObject.MultiReturn();
+            ChipLib.Log(multiReturn.Item1);
+            ChipLib.Log(multiReturn.Item2);
+            ExecFlow.current.Merge(rrcg_return_flow);
+        }
+    }
+
+    public class StudioFunctionsObject : StudioObjectBuilder
+    {
+        public StudioFunctionsObject(StudioObjectPort target): base(target, "TestPrefab")
+        {
+        }
+
+        [ExistingStudioFunction]
+        public void VoidFunction()
+        {
+            __SpawnStudioFunctionChip("VoidFunction");
+        }
+
+        [ExistingStudioFunction]
+        public void AllParameterTypes(BoolPort p0, FloatPort p1, IntPort p2, StringPort p3, ColorPort p4, Vector3Port p5, QuaternionPort p6)
+        {
+            __SpawnStudioFunctionChip("AllParameterTypes", p0, p1, p2, p3, p4, p5, p6);
+        }
+
+        [ExistingStudioFunction]
+        public Vector3Port SingleReturn(Vector3Port input)
+        {
+            __SpawnStudioFunctionChip("SingleReturn", input);
+            var node = Context.lastSpawnedNode;
+            return new Vector3Port()
+            {Port = node.Port(0, 1)};
+        }
+
+        [ExistingStudioFunction]
+        public (Vector3Port, QuaternionPort) MultiReturn()
+        {
+            __SpawnStudioFunctionChip("MultiReturn");
+            var node = Context.lastSpawnedNode;
+            return (new Vector3Port()
+            {Port = node.Port(0, 1)}, new QuaternionPort()
+            {Port = node.Port(0, 2)});
+        }
+
+        public override void __UpdateStudioEvents()
+        {
+            __GetImplementation().__ClearEvents(this);
+            __GetImplementation().__FinalizeEvents(this);
+        }
+    }
+}
