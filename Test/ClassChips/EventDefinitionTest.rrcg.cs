415ade1b8f4d904ad7d52edd4441a6da49a13c4d
notrabs <35877960+notrabs@users.noreply.github.com>
2023-11-17 17:50:42 +0100

    Add EventDefintion test

---
+++ Test/ClassChips/EventDefinitionTest.rrcg.cs
@@ -0,0 +1,57 @@
+﻿using RRCGSource;
+
+internal class EventDefintionTest : CircuitDescriptor
+{
+    public override void CircuitGraph()
+    {
+        ParamCounts();
+
+        SendTypes();
+
+        Existing();
+    }
+
+    void ParamCounts()
+    {
+        var params0 = new EventDefinition();
+        params0.Receiver();
+        params0.SendLocal();
+
+        var params1 = new EventDefinition<int>("param0");
+        var params1Value = params1.Receiver();
+        params1.SendLocal(params1Value);
+
+        var params2 = new EventDefinition<string, bool>("param0", "param1");
+        var params2Value = params2.Receiver();
+        params2.SendLocal(params2Value.Item1, params2Value.Item2);
+    }
+
+    void SendTypes()
+    {
+        var params0 = new EventDefinition();
+        params0.Receiver();
+        params0.SendLocal();
+        params0.SendOthers();
+        params0.SendAll();
+        params0.SendAuthority();
+        params0.SendRoomAuthority();
+        params0.SendPlayer(Player.Local);
+        params0.SendPlayer(GetLocalPlayer());
+    }
+
+    void Existing()
+    {
+        var params0 = ExistingEvent("Existing0");
+        params0.Receiver();
+        params0.SendLocal(); 
+
+        var params1 = ExistingEvent<int>("Existing1");
+        var params1Value = params1.Receiver();
+        params1.SendLocal(params1Value);
+
+        var params2 = ExistingEvent<int, string>("Existing2");
+        var params2Value = params2.Receiver();
+        params2.SendLocal(params2Value.Item1, params2Value.Item2);
+    }
+}
+
---
+++ Test/ClassChips/EventDefinitionTest.rrcg.gen.cs
@@ -0,0 +1,62 @@
+using RRCGBuild;
+using System.Collections.Generic;
+
+namespace RRCGBuild
+{
+    internal class EventDefintionTest : CircuitBuilder
+    {
+        public override void CircuitGraph()
+        {
+            ExecFlow rrcg_return_flow = new ExecFlow();
+            ParamCounts();
+            SendTypes();
+            Existing();
+            ExecFlow.current.Merge(rrcg_return_flow);
+        }
+
+        void ParamCounts()
+        {
+            ExecFlow rrcg_return_flow = new ExecFlow();
+            var params0 = new EventDefinition();
+            params0.Receiver();
+            params0.SendLocal();
+            var params1 = new EventDefinition<IntPort>("param0");
+            var params1Value = params1.Receiver();
+            params1.SendLocal(params1Value);
+            var params2 = new EventDefinition<StringPort, BoolPort>("param0", "param1");
+            var params2Value = params2.Receiver();
+            params2.SendLocal(params2Value.Item1, params2Value.Item2);
+            ExecFlow.current.Merge(rrcg_return_flow);
+        }
+
+        void SendTypes()
+        {
+            ExecFlow rrcg_return_flow = new ExecFlow();
+            var params0 = new EventDefinition();
+            params0.Receiver();
+            params0.SendLocal();
+            params0.SendOthers();
+            params0.SendAll();
+            params0.SendAuthority();
+            params0.SendRoomAuthority();
+            params0.SendPlayer(PlayerPort.Local);
+            params0.SendPlayer(GetLocalPlayer());
+            ExecFlow.current.Merge(rrcg_return_flow);
+        }
+
+        void Existing()
+        {
+            ExecFlow rrcg_return_flow = new ExecFlow();
+            var params0 = ExistingEvent("Existing0");
+            params0.Receiver();
+            params0.SendLocal();
+            var params1 = ExistingEvent<IntPort>("Existing1");
+            var params1Value = params1.Receiver();
+            params1.SendLocal(params1Value);
+            var params2 = ExistingEvent<IntPort, StringPort>("Existing2");
+            var params2Value = params2.Receiver();
+            params2.SendLocal(params2Value.Item1, params2Value.Item2);
+            ExecFlow.current.Merge(rrcg_return_flow);
+        }
+    }
+}
