ff3b94070ca2e1a7fa68b8d69ed6d990552e56d1
nulln <nullnemail@gmail.com>
2024-01-08 17:42:28 +1000

    Adjust parameter typing for TestEvent

--- Runtime/Types/Events/RoomEvent.cs
+++ Runtime/Types/Events/RoomEvent.cs
@@ -9,8 +9,9 @@ namespace RRCGSource
         public static float Hz30() => default;
         public static void Hz30(AlternativeExec<float> onHz30) { }
 
-        public static void TestEvent() { }
-        public static void TestEvent(AlternativeExec<float> onTestEvent) { }
+        /// <returns>The parameter configured by the user before sending Test Event</returns>
+        public static string TestEvent() => default;
+        public static void TestEvent(AlternativeExec<string> onTestEvent) { }
 
         /// <returns>The consumable requested for use by the user</returns>
         public static Consumable ConsumableUsed() => default;
--- Runtime/Types/Events/RoomEventBuild.cs
+++ Runtime/Types/Events/RoomEventBuild.cs
@@ -7,8 +7,8 @@ namespace RRCGBuild
         public static FloatPort Hz30() => ChipBuilder.EventReceiver<FloatPort>(RoomEvents.Hz30);
         public static void Hz30(AlternativeExec<FloatPort> onHz30) => CircuitBuilder.InlineGraph(() => onHz30(Hz30()));
 
-        public static void TestEvent() => ChipBuilder.EventReceiver(RoomEvents.TestEvent);
-        public static void TestEvent(AlternativeExec onTestEvent) => CircuitBuilder.InlineGraph(() => { TestEvent(); onTestEvent(); });
+        public static StringPort TestEvent() => ChipBuilder.EventReceiver<StringPort>(RoomEvents.TestEvent);
+        public static void TestEvent(AlternativeExec<StringPort> onTestEvent) => CircuitBuilder.InlineGraph(() => { onTestEvent(TestEvent()); });
 
         public static ConsumablePort ConsumableUsed() => ChipBuilder.EventReceiver<ConsumablePort>(RoomEvents.ConsumableUsed);
         public static void ConsumableUsed(AlternativeExec<ConsumablePort> onConsumableUsed) => CircuitBuilder.InlineGraph(() => onConsumableUsed(ConsumableUsed()));
