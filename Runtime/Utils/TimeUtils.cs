6ef354630cccd5bd64f2bb6232c9bcee12ae77f4
notrabs <35877960+notrabs@users.noreply.github.com>
2024-10-12 22:48:48 +0200

    Use more accurate profiling timer

--- Runtime/Utils/TimeUtils.cs
+++ Runtime/Utils/TimeUtils.cs
@@ -1,23 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace RRCG
 {
     // A more convenient stopwatch
     public class ProfilingTimer
     {
-        DateTime startTime = DateTime.Now;
-        public double SecondsElapsed => (DateTime.Now - startTime).TotalSeconds;
+        Stopwatch sw;
+
+        public ProfilingTimer()
+        {
+            sw = new Stopwatch();
+            sw.Start();
+        }
+
+        public double SecondsElapsed => sw.Elapsed.TotalSeconds;
         public string Formatted { get => $"{SecondsElapsed:f2}s"; }
 
         /// <summary> Returns the current time and resets afterwards </summary>
         public string StartNew()
         {
             var current = Formatted;
-            startTime = DateTime.Now;
+            sw.Restart();
             return current;
         }
     }
